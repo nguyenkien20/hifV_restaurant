7e5211aad7abd9321140c5252c67c0657b9fba20
Nguyen Kien <kiennguyendai2005@gmail.com>
2026-09-01 01:32:42 +0700

    first upload

---
+++ admin.h
@@ -0,0 +1,92 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#include <stdint.h>
+#include <inttypes.h>
+
+#define PHONENUMBER_LENG 11 // one character for NULL
+#define MAX_CHAR 20
+#define MAX_NAME 50
+#define MAX_TABLE 20
+
+
+typedef struct Admin admin_t;
+typedef struct Item item_t;
+typedef struct Customer customer_t;
+typedef struct RolePanel role_t;
+
+struct RolePanel
+{
+    const char *label; // function name
+    void (*handler)(); // function pointer
+};
+
+struct Admin
+{
+    char username[MAX_NAME];
+    char password[MAX_NAME];
+};
+struct Item
+{
+    char id[MAX_CHAR];   // ma san pham
+    char name[MAX_NAME]; // ten san pham
+    double price;        // gia nhap
+    int quantity;
+};
+struct Customer
+{
+    char id[MAX_CHAR];
+    char name[MAX_NAME];
+    char phone_number[PHONENUMBER_LENG];
+    int people_number;
+    int is_reserved;
+    char table_id[MAX_TABLE];
+};
+struct Discount
+{
+    customer_t guest; //discount add depend on guest.id
+    double percent;
+};
+void main_board(); // x
+
+// for function pointer replace switch
+void menu_display(const role_t *pMenu, int size);
+void menu_choose(const role_t *pMenu, uint8_t choose_index);
+
+// ADMIN
+void adminPanel();                // (x) admin panel
+int admin_login(admin_t *pAdmin); // (x) create and revised account or password
+void admin_account_create();
+// CUSTOMER
+void customerPanel();
+void customer_login();
+void customer_create_account();
+// CUSTOMER CONTROLLER
+void reserve_table();
+void see_my_reservation();
+void customer_reserved_update();
+void customer_reserved_cancel();
+void customer_menu_display();
+void view_available_tables();
+// ADMIN CONTROLLER
+//  main function inventory
+void inventory_manage(const role_t *pInventory, int size);     //////////
+void inventory_display();    // x
+void item_add(item_t *pAdd); // x
+void item_update();
+void item_remove();
+// customer manage main function
+void customer_manage(const role_t *pCustomer, int size); ///////////
+void customer_add();
+void customer_display();
+void customer_remove();
+void admin_view_customer_reserved();
+// discount manage
+void discount_manage(const role_t *pDiscount, int size); ////////////
+void discount_add();
+void discount_remove();
+void discount_display();
+// record
+void meal_record(); ///////////////
+void item_current(); //////////
+#endif
---
+++ main.c
@@ -0,0 +1,336 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h> // time for check in check out
+#include "admin.h"
+
+#define INVENTORY_SIZE (sizeof(inventory_manage_admin) / sizeof(inventory_manage_admin[0]))
+#define CUSTOMER_SIZE (sizeof(customer_manage_admin) / sizeof(customer_manage_admin[0]))
+#define DISCOUNT_SIZE (sizeof(discount_manage_admin) / sizeof(discount_manage_admin[0]))
+#define CUSTOMER_PANEL (sizeof(customer_view) / sizeof(customer_view[0]))
+
+// inventory_admin_views
+const role_t inventory_manage_admin[4] = {{"Open inventory display", inventory_display},
+                                          {"Add more item", item_add},
+                                          {"Update item", item_update},
+                                          {"Remove item", item_remove}};
+// customer_admin_view
+const role_t customer_manage_admin[4] = {{"Display all customers", customer_display},
+                                         {"Add customer", customer_add},
+                                         {"Remove customer", customer_remove},
+                                         {"Customer's reserve", admin_view_customer_reserved}};
+// discount_admin_view
+const role_t discount_manage_admin[3] = {{"Display all discount", discount_display},
+                                         {"Add discount", discount_add},
+                                         {"Remove discount", discount_remove}};
+/*CUSTOMER RESERVATION PANEL*/
+const role_t customer_view[6] = {{"Reserved my table", reserve_table},
+                                 {"See my reservation", see_my_reservation},
+                                 {"Update my reservation", customer_reserved_update},
+                                 {"Cancel my reservation", customer_reserved_cancel},
+                                 {"Display menu", customer_menu_display},
+                                 {"View available table", view_available_tables}};
+
+int main()
+{
+    main_board();
+    return 0;
+}
+void main_board()
+{
+    char role[10];
+    printf("---------------------------------\n");
+    printf("Welcome to the Hiff-V restaurant!\n");
+    printf("---------------------------------");
+    printf("\nYou are??\n");
+    printf("1. Admin (enter admin)\n");
+    printf("2. Customer (enter customer)\n");
+    printf("3. Enter 3 to exit the program\n");
+    do
+    {
+        printf("Enter your choice: \n");
+        scanf("%s", role);
+        if (strcmp(role, "admin") == 0)
+        {
+            adminPanel();
+        }
+        else if ((strcmp(role, "customer") == 0))
+        {
+            customerPanel();
+        }
+        else if (strcmp(role, "3") == 0)
+        {
+            printf("EXIT THE PROGRAM!!\n");
+        }
+        if (strcmp(role, "admin") != 0 && strcmp(role, "customer") != 0 && strcmp(role, "3") != 0)
+        {
+            printf("please try again!!\n");
+        }
+    } while (strcmp(role, "admin") != 0 && strcmp(role, "customer") != 0 && strcmp(role, "3") != 0);
+}
+///////////////////////////////////////////////////////////////////
+void adminPanel()
+{
+    int flag = 0;
+    admin_t admin1; // max admin account is 10
+    unsigned int choose1 = 0, choose2 = 0;
+    printf("--------------------------");
+    printf("\nThis is the admin panel!\n");
+    printf("--------------------------\n");
+    printf("\n1.Sign in?\n");
+    printf("2.Create a new admin account!\n");
+    printf("3. Back to main board\n");
+    do
+    {
+        printf("enter your choice:\n");
+        scanf("%u", &choose1);
+        if (choose1 == 1)
+        {
+            flag = admin_login(&admin1);
+            break;
+        }
+        else if (choose1 == 2)
+        {
+            admin_account_create();
+        }
+        else if (choose1 == 3)
+        {
+            main_board();
+        }
+        else
+        {
+            printf("Must choose 1, 2 or 3!!\n");
+        }
+    } while (choose1 < 1 || choose1 > 3);
+    if (flag == 1)
+    {
+        printf("------ADMIN MENU------\n");
+        printf("1. Manage inventory!\n");
+        // void inventory_manage();
+        printf("2. Manage customers!\n");
+        // void customer_manage();
+        printf("3. Manage discount!\n");
+        // void discount_manage();
+        printf("4. Change admin credentials!\n");
+        printf("5. Exit admin menu\n");
+        // void main_board(); // return to main board
+        printf("Make your choice !\n");
+        scanf("%d", &choose2);
+    }
+    switch (choose2)
+    {
+    case 1:
+        inventory_manage(inventory_manage_admin, INVENTORY_SIZE);
+        break;
+    case 2:
+        customer_manage(customer_manage_admin, CUSTOMER_SIZE);
+        break;
+    case 3:
+        discount_manage(discount_manage_admin, DISCOUNT_SIZE);
+        break;
+    case 4:
+        admin_login(&admin1);
+        break;
+    default:
+        printf("Exit the program\n");
+        break;
+    }
+}
+// customer ///
+///////////////////////////////////////////////////////////////////
+void customerPanel()
+{
+    unsigned int choose1 = 0, choose2 = 0;
+    printf("--------------------------");
+    printf("\nThis is the customer panel!\n");
+    printf("--------------------------\n");
+    printf("1.Sign in?\n");
+    printf("2.Create a new customer account!\n");
+    printf("3. Exit to home page\n");
+    do
+    {
+        printf("enter your choice:\n");
+        scanf("%u", &choose1);
+        if (choose1 == 1)
+        {
+            customer_login();
+            break;
+        }
+        else if (choose1 == 2)
+        {
+            customer_create_account();
+        }
+        else if (choose1 == 3)
+        {
+            // main_board();
+        }
+        else
+        {
+            printf("Must choose 1, 2 or 3!!\n");
+        }
+    } while (choose1 < 1 || choose1 > 3);
+    printf("Login succesfully!!!\n");
+    menu_display(customer_view, CUSTOMER_PANEL);
+}
+///////////////////////////////////////////////////////////////////
+void item_add(item_t *pAdd)
+{
+    int quantity = 0;
+    printf("Enter a item's number to store:\n");
+    scanf("%d", &quantity);
+    for (int i = 0; i < quantity; i++)
+    {
+        printf("Enter item %d's id: \n", i + 1);
+        scanf("%s", (pAdd + i)->id);
+        printf("Enter item %d's name: \n", i + 1);
+        scanf("%s", (pAdd + i)->name);
+        printf("Enter item %d's price: \n", i + 1);
+        scanf("%lf", &((pAdd + i)->price));
+        printf("Enter item %d's quantity: \n", i + 1);
+        scanf("%d", &((pAdd + i)->quantity));
+    }
+    printf("\nSTT\tID\tNAME\tPRICE\tQUANTITY\n");
+    for (int i = 0; i < quantity; i++)
+    {
+        printf("%d\t%s\t%s\t%.2lf\t%d\n", i + 1, *((pAdd + i)->id), *((pAdd + i)->name), ((pAdd + i)->price), ((pAdd + i)->quantity));
+    }
+}
+void inventory_display()
+{
+    printf("Success\n");
+    printf("Those item in store is:\n");
+}
+
+int admin_login(admin_t *pAdmin)
+{
+    int flag = 0;
+    item_t items[100]; // list maximum 100 items
+    printf("---Authentication For Admin---\n");
+    // must create a logic login
+    printf("enter admin username:\n");
+    // fgets(username, sizeof(username), stdin); co the su dung de nhap chuoi
+    // username[ strcspn( username, "\n" ) ] = '\0';
+    scanf("%s", (pAdmin)->username);
+    printf("enter admin password:\n");
+    scanf("%s", (pAdmin)->password);
+    flag = 1; // mean login successful
+    return flag;
+    // item_add(items); // test function
+}
+void customer_login()
+{
+    printf("---Authentication For Customer---\n");
+    printf("enter a customer username:\n");
+    printf("enter customer password:\n");
+}
+
+void admin_account_create()
+{
+    printf("enter an admin account name: \n");
+    printf("enter a password\n");
+    printf("enter one again password\n");
+}
+
+void customer_create_account()
+{
+    printf("enter an customer account name: \n");
+    printf("enter a password\n");
+    printf("enter one again password\n");
+}
+void menu_display(const role_t *pMenu, int size)
+{
+    uint8_t choose_index = 0;
+    printf("\n------PANEL CONTROL------\n");
+    for (uint8_t i = 0; i < size; i++)
+    {
+        printf("%d. %s\n", i + 1, (pMenu + i)->label);
+    }
+    printf("Enter a number to open: \n");
+    while (scanf("%" SCNu8, &choose_index) != 1 || choose_index < 1 || choose_index > size) // nhap gia tri thanh cong ham scanf tra ve 1
+    {
+        printf("Please enter value form %d to %d\n", 1, size);
+        while (getchar() != '\n')
+            ;
+    }
+    // user enter choose_index = 3;
+    menu_choose(pMenu, choose_index);
+}
+void menu_choose(const role_t *pMenu, uint8_t choose_index)
+{
+    choose_index -= 1;                 // minux 1 because choose_index != array implement_index
+    (pMenu + choose_index)->handler(); // <----- remember have ()
+}
+void item_update()
+{
+}
+void item_remove()
+{
+}
+void customer_display()
+{
+}
+void customer_add()
+{
+}
+void customer_remove()
+{
+}
+void discount_display()
+{
+}
+void discount_add()
+{
+}
+void discount_remove()
+{
+}
+void reserve_table()
+{
+}
+void see_my_reservation()
+{
+}
+void customer_reserved_update()
+{
+}
+void customer_reserved_cancel()
+{
+}
+void customer_menu_display()
+{
+}
+void view_available_tables()
+{
+}
+void inventory_manage(const role_t *pInventory, int size)
+{
+    printf("INVENTORY MANAGEMENT ADMIN'S VIEW");
+    menu_display(pInventory, size);
+}
+void customer_manage(const role_t *pCustomer, int size)
+{
+    printf("CUSTOMER MANAGEMENT ADMIN'S VIEW");
+    menu_display(pCustomer, size);
+}
+void admin_view_customer_reserved()
+{
+}
+void discount_manage(const role_t *pDiscount, int size)
+{
+    printf("DISCOUNT MANAGEMENT ADMIN'S VIEW");
+    menu_display(pDiscount, size);
+}
+void item_current()
+{
+    int item_index = 0;
+    printf("Enter the current item in restaurant still available: \n");
+    scanf("%d", &item_index);
+    int *pSeat = NULL;
+    pSeat = (int*)calloc(item_index, sizeof(int));
+    if(pSeat == NULL){
+        printf("Cant allocate memory!\n");
+    }
+
+    free(pSeat);
+    pSeat = NULL;
+}
