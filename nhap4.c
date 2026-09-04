09ce07d2e1a134faef1b0f552810169fcb31f274
Nguyen Kien <kiennguyendai2005@gmail.com>
2026-09-04 23:34:43 +0700

    test on test1 branch

--- nhap4.c
+++ nhap4.c
@@ -8,7 +8,7 @@
 #define MAX_NAME 50
 #define MAX_TABLE 20
 #define MAX_CUSTOMER 100
- 
+
 typedef struct Customer
 {
     char id[MAX_CHAR];
@@ -19,17 +19,25 @@ typedef struct Customer
     int table_index;
 } customer_t;
 
-void print_information_customer(customer_t customer1, FILE *pFile, int customer_index);
+void get_information_customer(customer_t customer1, FILE *pFile, int customer_index);
 int customer_add(void);
+void printf_customer_information(customer_t customer1, FILE *pFile, int customer_index);
 
 int main()
 {
     // if state is main_admin_panel_add_customer
-    customer_add();
-    return 0;
+    int value = customer_add();
+    if(value == -1){
+        printf("EXIT THE PROGRAM!!\n");
+        return (-1);
+    }
+    else{
+        return 0;
+    }
 }
-void print_information_customer(customer_t customer1, FILE *pFile, int customer_index){
-     printf("Enter a id table number: \n");
+void get_information_customer(customer_t customer1, FILE *pFile, int customer_index)
+{
+    printf("Enter a id table number: \n");
     scanf("%d", &customer1.table_index);
     while (getchar() != '\n')
         ;
@@ -47,16 +55,15 @@ void print_information_customer(customer_t customer1, FILE *pFile, int customer_
     fgets(customer1.phone_number, PHONENUMBER_LENG, stdin);
     customer1.phone_number[strcspn(customer1.phone_number, "\n")] = '\0';
 
-
     printf("Enter customer's quantity (/1 table): \n");
     scanf("%d", &customer1.people_number);
     while (getchar() != '\n')
         ;
-    fprintf(pFile, "\n-----THIS IS CUSTOMER/%d/-----\n", customer_index);
-    fprintf(pFile, "1. ID_TABLE: %d\n2. ID_CUSTOMER: %s\n3. CUSTOMER NAME: %s\n4. CUSTOMER PHONENUMBER: %s\n5. QUANTITY: %d\n", customer1.table_index, customer1.id, customer1.name, customer1.phone_number, customer1.people_number);
-    fprintf(pFile, "--------------------------------\n");
+    printf_customer_information(customer1, pFile, customer_index);
+
 }
-int customer_add(void){
+int customer_add(void)
+{
     int customer_index = 0;
     customer_t customers[MAX_CUSTOMER];
     FILE *pFile = fopen(CUSTOMER_LIST, "a");
@@ -67,14 +74,26 @@ int customer_add(void){
         return (-1);
     }
     printf("ENTER NUMBER INDEX OF CUSTOMER: \n");
-    while(scanf("%d", &customer_index) != 1 || customer_index < 1 || customer_index > 100){
+    while (scanf("%d", &customer_index) != 1 || customer_index < 1 || customer_index > 100)
+    {
         printf("PLEASE ENTER A NUMBER FROM 1 - 100!!!\n");
     }
     // admin enter 1 -> element [0];
     // admin enter 8 -> elenment [7];
     customer_index -= 1;
-    print_information_customer(customers[customer_index], pFile, customer_index + 1);
-   
+    get_information_customer(customers[customer_index], pFile, customer_index + 1);
     fclose(pFile);
     pFile = NULL;
+    return 1;
+}
+void printf_customer_information(customer_t customer1, FILE *pFile, int customer_index)
+{
+    int total_character = 0;
+    total_character += fprintf(pFile, "\n-------THIS IS CUSTOMER/%d/-------\n", customer_index);
+    total_character += fprintf(pFile, "1. ID_TABLE: %d\n2. ID_CUSTOMER: %s\n3. CUSTOMER NAME: %s\n4. CUSTOMER PHONENUMBER: %s\n5. QUANTITY: %d\n", customer1.table_index, customer1.id, customer1.name, customer1.phone_number, customer1.people_number);
+    total_character += fprintf(pFile, "--------------------------------\n");
+
+    printf("LENGTH IS: %d\n", total_character);
+    // printf("Distance from last position to current is: %d bytes\n", length - last_value);
+    fseek(pFile, - total_character, SEEK_CUR);
 }
